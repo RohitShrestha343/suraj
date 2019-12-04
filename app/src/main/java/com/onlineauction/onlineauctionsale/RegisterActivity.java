a33c7eff1c4ba0bea8e87984b6c0ff45ed7d3dda
Rohit Shrestha <rohit.xresta@gmail.com>
2019-12-04 19:21:36 +0545

    Camera Implementation

--- app/src/main/java/com/onlineauction/onlineauctionsale/RegisterActivity.java
+++ app/src/main/java/com/onlineauction/onlineauctionsale/RegisterActivity.java
@@ -1,15 +1,56 @@
 package com.onlineauction.onlineauctionsale;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
-
+private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         getSupportActionBar().setTitle("Register Form");
+        imageView=findViewById(R.id.imageview);
+        CheckPermission();
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoadCamera();
+            }
+        });
+    }
+    private void LoadCamera(){
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(intent,0);
+        }
+    }
+    private void CheckPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]
+                    {Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},0);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==0 && resultCode==RESULT_OK){
+            Bundle extras=data.getExtras();
+            Bitmap imageBitMap=(Bitmap)extras.get("data");
+            imageView.setImageBitmap(imageBitMap);
+        }
+        //super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
