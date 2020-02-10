cea5a9735e1cb7fae02f0f090a7853eeafffff3a
Rohit Shrestha <rohit.xresta@gmail.com>
2020-02-10 14:28:48 +0545

    SearchTesting

---
+++ app/src/androidTest/java/com/onlineauction/onlineauctionsale/Search_Testing.java
@@ -0,0 +1,25 @@
+package com.onlineauction.onlineauctionsale;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+public class Search_Testing {
+    @Rule
+    public ActivityTestRule<SearchActivity> activityTestRule = new ActivityTestRule<>(SearchActivity.class );
+    @Test
+    public void loginTest(){
+        onView(withId(R.id.etCity)).perform(typeText( "Ashirvad Boarding School" ));
+        closeSoftKeyboard();
+
+        onView( withId( R.id.btnSearch ) ).perform( click() );
+
+    }
+}
